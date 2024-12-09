4be8de84a6dbaa58353daedf0d76ffc7bd529aa3
Gabriel Costa <gabrielcg@macbookpro.home>
2024-12-09 08:53:09 +0000

    Add border to image

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -5,7 +5,7 @@ export const Thanks = () => {
     <section className='p-10 md:p-12 bg-black text-white'>
       <div className='mx-auto my-0 max-w-screen-xl flex flex-col items-center'>
         <p className='text-3xl md:text-7xl lg:text-8xl text-center mb-10 font-anton'>Thanks for visiting, here's a cat!</p>
-        <picture>
+        <picture className='border-4 border-white rounded'>
           <img width={600} src='https://cataas.com/cat?height=600' alt='Cat'  />
         </picture>
       </div>
