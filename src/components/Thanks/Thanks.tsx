8e8f4ddf983b87fd5183d4cd25ab94fbec8cfc97
Gabriel Costa <gabrielcg@Gabriels-MacBook-Pro.local>
2024-12-12 10:30:44 +0000

    Update cat api

--- src/components/Thanks/Thanks.tsx
+++ src/components/Thanks/Thanks.tsx
@@ -6,7 +6,7 @@ export const Thanks = () => {
       <div className='mx-auto my-0 max-w-screen-xl flex flex-col items-center'>
         <p className='text-3xl md:text-7xl lg:text-8xl text-center mb-10 font-anton'>Thanks for visiting, here's a cat!</p>
         <picture className='border-4 border-white rounded'>
-          <img width={600} src='https://cataas.com/cat?height=600' alt='Cat'  />
+          <img width={600} src='https://cattos.xyz/api' alt="Here's a Cat"  />
         </picture>
       </div>
     </section>
