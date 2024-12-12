cb0043b32178f560de707ea76ff3e9cc604d2af9
Gabriel Costa <gabrielcg@Gabriels-MacBook-Pro.local>
2024-12-12 10:53:22 +0000

    Adjust icon path again

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -13,8 +13,8 @@ export const SEO = (props: Props) => {
   const seo = {
     title: title || 'Gabriel Costa - Web Developer',
     description: description || 'Hi, come take a look at my projects and professional experience!',
-    image: image || '/images/og-image.jpg',
-    icon: '/images/icon.png',
+    image: image || '/static/images/og-image.jpg',
+    icon: '/static/images/icon.png',
     url: 'https://gabrielc.dev',
     twitterUsername: 'thatgabrielc',
   }
