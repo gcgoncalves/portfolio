9cddd921f561a60d8b421955f044eb59a4a21e44
Gabriel Costa <gabrielcg@Gabriels-MacBook-Pro.local>
2024-12-12 07:59:00 +0000

    Fix SEO minor issues

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer className='p-5 bg-black w-full'>
-      <div className='flex flex-row justify-end  py-0 md:py-5 px-5 gap-5' aria-label='Visit my github profile'>
-        <a href='https://github.com/gcgoncalves' target='_blank'>
+      <div className='flex flex-row justify-end  py-0 md:py-5 px-5 gap-5'>
+        <a href='https://github.com/gcgoncalves'  target='_blank' aria-label='Visit my github profile'>
           <div className=' h-8 w-8 bg-cover bg-[url("/images/github.svg")] hover:bg-[url("/images/github-yellow.svg")]'></div>
         </a>
         <a href='https://linkedin.com/in/gcgoncalves' target='_blank' aria-label='Visit my linkedin profile'>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -13,10 +13,10 @@ export function Hero(props: {lenis: Lenis | null}) {
         </div>
         <div className='pt-8 md:pt-0 md:pb-0 md:pl-16 font-bold border-t-4 md:border-t-0 md:border-l-4 border-black border-solid'>
           <menu className='h-full grid grid-cols-1 gap-1 md:gap-4 place-content-around'>
-            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#about')} aria-label='About'>About</a>
-            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#tech-stack')} aria-label='Technology'>Technology</a>
-            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#experience')} aria-label='Experience'>Experience</a>
-            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#projects')} aria-label='Projects'>Projects</a>
+            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#about')} href='#about' aria-label='About'>About</a>
+            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#tech-stack')} href='#tech-stack' aria-label='Technology'>Technology</a>
+            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#experience')} href='#experience' aria-label='Experience'>Experience</a>
+            <a className='text-xl md:text-3xl lg:text-5xl cursor-pointer hover:underline' onClick={() => lenis?.scrollTo('#projects')} href='#projects' aria-label='Projects'>Projects</a>
           </menu>
         </div>
       </div>
