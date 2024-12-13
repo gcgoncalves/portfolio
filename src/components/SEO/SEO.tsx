dd63dab695f4e2a7708518a5258ab8a4253d02b4
Gabriel Costa <gabrielcg@Gabriels-MacBook-Pro.local>
2024-12-13 07:39:25 +0000

    Add secure url to og images

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -27,9 +27,11 @@ export const SEO = (props: Props) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta property="og:image" content={seo.image} itemProp="image" />
+      <meta property="og:image:secure_url" content={`${seo.url}${seo.image}`} /> 
       <meta property="og:image:width" content="1280"/>
       <meta property="og:image:height" content="720"/>
       <meta property="og:image" content={seo.imageWpp} itemProp="image" />
+      <meta property="og:image:secure_url" content={`${seo.url}${seo.imageWpp}`} /> 
       <meta property="og:image:width" content="400"/>
       <meta property="og:image:height" content="400"/>
       <meta name="og:url" content={seo.url} />
@@ -37,7 +39,9 @@ export const SEO = (props: Props) => {
       <meta property="og:description" content={seo.description} />
       <meta name="og:type" content="website" />
       <meta property="og:locale" content="en_GB" />
+      <meta property="og:updated_time" content="202412130740" />
       <meta name="twitter:card" content={seo.image} itemProp="image" />
+      <meta name="twitter:card:secure_url" content={`${seo.url}${seo.image}`} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
