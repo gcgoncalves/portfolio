1abedd17b438be52a2d1f23583b30ec73fa8e173
Gabriel Costa <gabrielcg@Gabriels-MacBook-Pro.local>
2024-12-14 08:24:40 +0000

    Use absolute path for og images

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -14,9 +14,9 @@ export const SEO = (props: Props) => {
   const seo = {
     title: title || 'Gabriel Costa - Web Developer',
     description: description || 'Hi, come take a look at my projects and professional experience!',
-    image: image || '/og-image.jpg',
-    imageWpp: imageWpp || '/og-image-wpp.jpg',
-    icon: '/icon.ico',
+    image: image || 'https://gabrielc.dev/og-image.jpg',
+    imageWpp: imageWpp || 'https://gabrielc.dev/og-image-wpp.jpg',
+    icon: 'https://gabrielc.dev/icon.ico',
     url: 'https://gabrielc.dev',
     twitterUsername: 'thatgabrielc',
   }
@@ -27,11 +27,11 @@ export const SEO = (props: Props) => {
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta property="og:image" content={seo.image} itemProp="image" />
-      <meta property="og:image:secure_url" content={`${seo.url}${seo.image}`} /> 
+      <meta property="og:image:secure_url" content={seo.image} /> 
       <meta property="og:image:width" content="1280"/>
       <meta property="og:image:height" content="720"/>
       <meta property="og:image" content={seo.imageWpp} itemProp="image" />
-      <meta property="og:image:secure_url" content={`${seo.url}${seo.imageWpp}`} /> 
+      <meta property="og:image:secure_url" content={seo.imageWpp} /> 
       <meta property="og:image:width" content="400"/>
       <meta property="og:image:height" content="400"/>
       <meta name="og:url" content={seo.url} />
@@ -41,7 +41,7 @@ export const SEO = (props: Props) => {
       <meta property="og:locale" content="en_GB" />
       <meta property="og:updated_time" content="202412130740" />
       <meta name="twitter:card" content={seo.image} itemProp="image" />
-      <meta name="twitter:card:secure_url" content={`${seo.url}${seo.image}`} />
+      <meta name="twitter:card:secure_url" content={seo.image} />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
